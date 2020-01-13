d859a7ec1a2fc700260093d58692f35eb260986a
Mohcka <bandagedwolf95@gmail.com>
2020-01-13 08:15:19 -0800

    updated comment

--- public/quick-scripts.js
+++ public/quick-scripts.js
@@ -1,5 +1,5 @@
 //* PIPELINE
-// Get business names from deals UI
+// Get business names from the pipelinedeals deals UI
 [...document.querySelectorAll(`td[data-column="deal_company_name"] a`)]
   .map(company_link => company_link.innerText)
   .join("\n");
