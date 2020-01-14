bab43749d74dbfe280027b661d122dcd1c65a0b9
Mohcka <bandagedwolf95@gmail.com>
2020-01-14 08:25:35 -0800

    Added prompts

--- index.js
+++ index.js
@@ -3,19 +3,26 @@ require("dotenv").config()
 const path = require("path")
 
 const webdriver = require("selenium-webdriver")
-const { Builder } = webdriver
-const firefox = require("selenium-webdriver/firefox")
 const chrome = require("selenium-webdriver/chrome")
-const geckoPath = require("geckodriver").path
 const chromePath = require("chromedriver").path
+
+const prompts = require("prompts")
 
 const { getData } = require("./src/featch-pipeline-data")
 const { runPreBuildout } = require("./src/pre-buildout-task")
+const { runWpPreconfig } = require("./src/wp-config-task")
 
 let driver = null
+let choice = 0
 
 // Initialize driver
 const runAutomation = async () => {
+  await promptUser()
+  await createWebDriver()
+  await run()
+}
+
+async function createWebDriver() {
   let service = await new chrome.ServiceBuilder(chromePath).build()
   chrome.setDefaultService(service)
 
@@ -23,7 +30,10 @@ const runAutomation = async () => {
     .withCapabilities(webdriver.Capabilities.chrome())
     .build()
 
-  await run()
+  await driver
+    .manage()
+    .window()
+    .setRect({ width: 1200, height: 880, x: 0, y: 0 })
 }
 
 // Start running automation tasks
@@ -34,8 +44,33 @@ const run = async () => {
   // 0. Get data
   plData = await getData()
   console.log(plData)
-  // 1. Prebuildout
-  await runPreBuildout(driver, plData)
+
+  // Perform acton based on choice
+  switch (choice) {
+    case 1:
+      await runPreBuildout(driver, plData)
+    case 2:
+      await runWpPreconfig(driver, plData)
+    default:
+      console.log(
+        "You've made an invalid input.  Please restart the program and try again."
+      )
+  }
+}
+
+async function promptUser() {
+  const choicesPromptText = `Which step would you like to perform.  The actions will perform for all deals you have
+  entered in the company_names.json file. (Select a Number):
+  1. Prebuildout
+  2. Configure Wordpress for deal
+  `
+  const choicePrompt = await prompts({
+    type: "number",
+    name: "choice",
+    message: choicesPromptText,
+  })
+
+  choice = choicePrompt.choice
 }
 
 module.exports.runAutomation = runAutomation()
